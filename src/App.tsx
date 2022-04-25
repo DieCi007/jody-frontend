4f77c71990786f7b95e119bc43cba4e1e1d4e1d1
Diego Cipa <diego@Diegos-MacBook-Pro.local>
2022-04-25 14:33:05 +0200

    Switched to default theme handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useMemo, useReducer, useState } from 'react';
+import React, { createContext, useMemo, useState } from 'react';
 import styles from './App.module.scss';
 import { createTheme, CssBaseline, PaletteMode, ThemeProvider, useMediaQuery } from '@mui/material';
 import { getDefaultThemeMode } from './shared/theme/default-theme-mode';
@@ -8,13 +8,22 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { OPTIONS } from './shared/i18n/translations';
-import { ColorModeContext, colorModeReducer } from './shared/ColorModeContext'
+
+export const ColorModeContext = createContext<{ toggleColor: () => void }>({
+    toggleColor: () => {
+    }
+});
 
 function App() {
     // THEME
     const prefersDarkMode: boolean = useMediaQuery('(prefers-color-scheme: dark)');
-    const themePalette = getDefaultThemeMode(prefersDarkMode);
-    const [theme, themeDispatch] = useReducer(colorModeReducer, createTheme(getDesignTokens(themePalette)));
+    const [themeMode, setThemeMode] = useState<'light' | 'dark'>('light');
+    const themeContext = useMemo(() => ({
+        toggleColor: () => {
+            setThemeMode((prev) => (prev === 'light' ? 'dark' : 'light'))
+        }
+    }), []);
+    const theme = useMemo(() => createTheme(getDesignTokens(themeMode)), [themeMode]);
 
     // LOAD LANGUAGE
     i18n.use(initReactI18next).init(OPTIONS);
@@ -23,7 +32,7 @@ function App() {
         <div className={styles.app} style={{background: theme.palette.background.default}}>
             <CssBaseline/>
             <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
-            <ColorModeContext.Provider value={{theme, themeDispatch}}>
+            <ColorModeContext.Provider value={themeContext}>
                 <ThemeProvider theme={theme}>
                     <Routes>
                         <Route path='/home' element={<Main/>}/>
--- src/main/components/theme-toggle/ThemeToggle.tsx
+++ src/main/components/theme-toggle/ThemeToggle.tsx
@@ -1,21 +1,22 @@
 import styles from './ThemeToggle.module.scss';
-import { createTheme, useTheme } from '@mui/material';
+import { useTheme } from '@mui/material';
 import { useContext } from 'react';
-import { ColorModeContext } from '../../../shared/ColorModeContext';
-import { getDesignTokens } from '../../../shared/theme/mui-design-tokens';
+import { ColorModeContext } from '../../../App';
+import { LS_THEME_MODE } from '../../../shared/constants';
 
 const ThemeToggle = () => {
-    const {theme, themeDispatch} = useContext(ColorModeContext);
-    const onThemeToggle = () => {
-        return themeDispatch &&
-            themeDispatch(createTheme(getDesignTokens(
-                theme?.palette?.mode === 'dark' ? 'light' : 'dark')))
+    const {palette} = useTheme();
+    const context = useContext(ColorModeContext);
+
+    const onChange = () => {
+        localStorage.setItem(LS_THEME_MODE, palette.mode === 'light' ? 'dark' : 'light')
+        context.toggleColor()
     }
     return (
         <>
             <label htmlFor='theme-toggle' className={`${styles.toggleBtn} 
-                   ${theme?.palette?.mode === 'dark' ? styles.dark : ''}`}
-                   onClick={() => onThemeToggle()}>
+                   ${palette.mode === 'dark' ? styles.dark : ''}`}
+                   onClick={() => onChange()}>
                 <input type='checkbox' id='theme-toggle' className={styles.input}/>
             </label>
         </>
