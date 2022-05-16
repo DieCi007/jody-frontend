fe6b31ad2b92c99547936a11c2a82bc26da3df8b
Diego Cipa <diego@7WK126-1203TR.group.pirelli.com>
2022-05-16 20:44:18 +0200

    Ease in text in homepage

--- src/main/homepage/Homepage.tsx
+++ src/main/homepage/Homepage.tsx
@@ -1,5 +1,5 @@
 import styles from './Homepage.module.scss'
-import Flicking, { MOVE_TYPE, WillChangeEvent } from '@egjs/react-flicking';
+import Flicking, { ChangedEvent, MOVE_TYPE, WillChangeEvent } from '@egjs/react-flicking';
 import React, { useEffect, useRef, useState } from 'react';
 import PaginationDots from './components/pagination-dots/PaginationDots';
 import RoundCard from './components/round-card/RoundCard';
@@ -29,6 +29,8 @@ const Homepage = () => {
     const [currentPage, setCurrentPage] = useState<number>(0);
     const flicking = useRef<Flicking>(null);
     const scene = useRef<AppScene>(null);
+    const [textServicesVisible, setTextServicesVisible] = useState<boolean>(false);
+    const [textEcoVisible, setTextEcoVisible] = useState<boolean>(false);
 
     useEffect(() => {
         if (flicking.current && flicking.current.initialized) {
@@ -62,21 +64,32 @@ const Homepage = () => {
         }
     }
 
-    const onFlickingChanged = (e: WillChangeEvent<Flicking>) => {
+    const onFlickingChanged = (e: ChangedEvent<Flicking>) => {
+        if (e.index === 1 && !textServicesVisible) {
+            return setTextServicesVisible(true);
+        }
+
+        if (e.index === 2 && !textEcoVisible) {
+            return setTextEcoVisible(true);
+        }
+    }
+
+    const onFlickingWillChange = (e: WillChangeEvent<Flicking>) => {
         setCurrentPage(e.index);
     }
 
-
     const onFlickingMove = () => {
         let progress = flicking.current?.camera.progress;
-        scene.current?.setTime(progress || 0)
+        progress = progress || 0;
+        scene.current?.setTime(progress)
+        return progress;
     }
 
     return (
         <div className={styles.homeWrapper}>
             <PaginationDots click={index => onPaginationClick(index)} current={currentPage} total={4}/>
             <div>
-                <AppScene keyframes={PROPS} time={0} ref={scene}>
+                <AppScene keyframes={PROPS} time={onFlickingMove()} ref={scene}>
                     <div className={`${styles.background} shapes`}>
                         <div className={`background2 ${styles.thirdPage}`}
                              style={{backgroundImage: palette.mode === 'light' ? `url(${srDay})` : `url(${srNight})`}}/>
@@ -105,12 +118,14 @@ const Homepage = () => {
                 </AppScene>
             </div>
             <Flicking ref={flicking} horizontal={false} autoResize={true} duration={700}
-                      onWillChange={e => onFlickingChanged(e)}
+                      onWillChange={e => onFlickingWillChange(e)} onChanged={(e) => onFlickingChanged(e)}
                       moveType={[MOVE_TYPE.STRICT, {count: 1}]}
                       bounce={10} onMove={onFlickingMove}>
                 <div className={styles.page}><HomepageTextMain/></div>
-                <div className={styles.page}><HomepageTextServices/></div>
-                <div className={styles.page}><HomepageTextEco/></div>
+                <div className={`${styles.page} ${styles.lazy} ${textServicesVisible ? styles.visible : ''}`}>
+                    <HomepageTextServices/></div>
+                <div className={`${styles.page} ${styles.lazy} ${textEcoVisible ? styles.visible : ''}`}>
+                    <HomepageTextEco/></div>
                 <div className={styles.page}><HomepageTextContact/></div>
             </Flicking>
         </div>
